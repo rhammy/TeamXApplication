58332ea10c3e4c5c16338872edc8b99467b23571
Reed Hamilton <hamiltonreed@ymail.com>
2017-03-21 15:35:18 +0800

    Use cases translated to classes.

---
+++ app/src/main/java/com/example/reedhamilton/teamxapplication/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.reedhamilton.teamxapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Reed Hamilton on 3/21/2017.
+ */
+
+public class DetailsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/example/reedhamilton/teamxapplication/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.example.reedhamilton.teamxapplication;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * Created by Reed Hamilton on 3/21/2017.
+ */
+
+public class RegistrationActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}
